385017862135cd6acd1107c3f40e7bef6ec48b13
Andrew Sepulveda <streak324@gmail.com>
2023-07-04 14:48:24 -0700

    add headless mode

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ NUM_GAMES = 1
 
 SNAKE_MODEL_FILEPATH = "./snakemodel.pth"
 
+HEADLESS = True
+
 class SnakeNeuralNet(nn.Module):
 	def __init__(self):
 		super().__init__()
@@ -178,6 +180,92 @@ class SnakeGame:
 			up_dist = y - self.snake[0][1]
 		return (up_dist, left_dist, down_dist, right_dist)
 
+class AgentSim:
+	def __init__(self, headless=False):
+		self.device = (
+			"cuda"
+			if torch.cuda.is_available()
+			else "mps"
+			if torch.backends.mps.is_available()
+			else "cpu"
+		)
+		self.neuralnet = SnakeNeuralNet()
+		if os.path.isfile(SNAKE_MODEL_FILEPATH):
+			self.neuralnet.load_state_dict(torch.load(SNAKE_MODEL_FILEPATH))
+			self.neuralnet.eval()
+		self.optimizer = torch.optim.SGD(self.neuralnet.parameters(), lr=1e-3)
+		self.snake_games = []
+		for i in range(NUM_GAMES):
+			self.snake_games.append(SnakeGame(BOARD_WIDTH, BOARD_HEIGHT))
+		self.start = time.time()
+		self.last_report = time.time()
+		self.max_steps = 0
+		self.max_snake_length = 0
+		self.num_restarts = 0
+		self.steps_accum = 0
+		snake_steps_progress_filepath = "./data/progress/{}_steps_progress.json".format(time.time_ns()/1_000_000_000)
+		self.steps_progress_file = open(snake_steps_progress_filepath, 'w', encoding="utf-8")
+		self.max_steps = 0
+		self.max_snake_length = 0
+
+	def update(self):
+		if COMPUTER_CONTROLLED:
+			self.apply_manual_step = False
+			input_matrix = torch.zeros((len(self.snake_games), 7)).float()
+			for i, game in enumerate(self.snake_games):
+				inputs = torch.tensor([game.food_pos[0] - game.snake[0][0], game.food_pos[1] - game.snake[0][1], game.snake_move_dir, 0, 0, 0, 0]).float()
+				obstacle_directions = game.get_obstacle_directions()
+				input_move_offset = 3
+				for j in range(4):
+					inputs[j+input_move_offset] = obstacle_directions[j]
+				input_matrix[i] = inputs
+			nn_out = self.neuralnet(input_matrix)
+			#actions = torch.max(nn_out, 1)[1]
+			#probability distribution function
+			pdf = torch.distributions.Categorical(nn_out)
+			actions = pdf.sample()
+			#actions = explore_actions.clone()
+			#for aidx, _ in enumerate(actions):
+			#	if random.random() > 0.25:
+			#		actions[aidx] = nn_out[aidx].max()
+			rewards = torch.zeros(len(self.snake_games))
+		for i, game in enumerate(self.snake_games):
+			if COMPUTER_CONTROLLED:
+				game.apply_move_dir(actions[i])
+			is_game_over = game.step()
+			self.max_snake_length = max(self.max_snake_length, len(game.snake))
+			self.max_steps = max(game.steps, self.max_steps)
+			if COMPUTER_CONTROLLED:
+				rewards[i] = calc_score(game)
+			if is_game_over:
+				if USER_INPUT_CONTROLLED:
+					return True
+				if COMPUTER_CONTROLLED:
+					self.snake_games[i] = SnakeGame(BOARD_WIDTH, BOARD_HEIGHT)
+					self.num_restarts += 1
+					self.steps_accum += game.steps
+					avg_steps = 0
+					if self.num_restarts % (NUM_GAMES * 10) == 0:
+						print("restarts: {}. accumulated steps: {}. elapsed time: {}. best episode: (steps: {}, snake size: {})".
+	    					format(self.num_restarts, self.steps_accum, time.time() - self.last_report, self.max_steps, self.max_snake_length))
+						self.last_report = time.time()
+						avg_steps = self.steps_accum / (NUM_GAMES * 10)
+						self.steps_accum = 0
+						data = {"steps": avg_steps, "restart":self.num_restarts}
+						json.dump(data, self.steps_progress_file)
+						self.steps_progress_file.write('\n')
+						self.steps_progress_file.flush()
+					if self.num_restarts % (NUM_GAMES * 100) == 0:
+						torch.save(self.neuralnet.state_dict(), SNAKE_MODEL_FILEPATH)
+		if COMPUTER_CONTROLLED:
+			loss = (-pdf.log_prob(actions) * rewards).sum()
+			loss.backward()
+			self.optimizer.step()
+			self.optimizer.zero_grad()
+			if HEADLESS == False:
+				print("possible actions: {}, chosen actions: {}. rewards: {}".format(nn_out, actions, rewards))
+		return False
+
 class MyWindow(arcade.Window):
 	def __init__(self):
 		self.fps = 0
@@ -190,17 +278,11 @@ class MyWindow(arcade.Window):
 		)
 		print(f"Using {self.device} device")
 		super().__init__(INITIAL_SCREEN_WIDTH, INITIAL_SCREEN_HEIGHT, SCREEN_TITLE, resizable=True)
+		self.set_vsync(False)
 		arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+		self.agent = AgentSim(False)
 
-		self.neuralnet = SnakeNeuralNet()
-		if os.path.isfile(SNAKE_MODEL_FILEPATH):
-			self.neuralnet.load_state_dict(torch.load(SNAKE_MODEL_FILEPATH))
-			self.neuralnet.eval()
-		self.optimizer = torch.optim.SGD(self.neuralnet.parameters(), lr=1e-3)
-		self.snake_games = []
 		self.game_view_idx = 0
-		for i in range(NUM_GAMES):
-			self.snake_games.append(SnakeGame(BOARD_WIDTH, BOARD_HEIGHT))
 		self.time_accum: float = 0
 
 		self.manager = arcade.gui.UIManager()
@@ -216,12 +298,6 @@ class MyWindow(arcade.Window):
 		self.are_steps_manual = False
 		self.apply_manual_step = True
 
-		self.num_restarts = 0
-		self.steps_accum = 0
-
-
-		snake_steps_progress_filepath = "./{}_steps_progress.json".format(time.time_ns()/1_000_000_000)
-		self.steps_progress_file = open(snake_steps_progress_filepath, 'w', encoding="utf-8")
 
 		for i in range(BOARD_HEIGHT):
 			board_color_idx = (board_color_idx + 1) % len(board_colors)
@@ -231,8 +307,6 @@ class MyWindow(arcade.Window):
 				sprite.center_y = board_y_start + i*PIXELS_PER_BOARD_TILE+PIXELS_PER_BOARD_TILE/2
 				self.spritelist.append(sprite)
 				board_color_idx = (board_color_idx + 1) % len(board_colors)
-		self.max_steps = 0
-		self.max_snake_length = 0
 
 	def setup(self):
 		pass
@@ -240,22 +314,22 @@ class MyWindow(arcade.Window):
 	def on_key_press(self, key: int, modifiers: int):
 		if USER_INPUT_CONTROLLED:
 			if key == arcade.key.UP:
-				self.snake_games[self.game_view_idx].apply_move_dir(SnakeGame.MOVE_UP)
+				self.agent.snake_games[self.game_view_idx].apply_move_dir(SnakeGame.MOVE_UP)
 			if key == arcade.key.LEFT:
-				self.snake_games[self.game_view_idx].apply_move_dir(SnakeGame.MOVE_LEFT)
+				self.agent.snake_games[self.game_view_idx].apply_move_dir(SnakeGame.MOVE_LEFT)
 			if key == arcade.key.DOWN:
-				self.snake_games[self.game_view_idx].apply_move_dir(SnakeGame.MOVE_DOWN)
+				self.agent.snake_games[self.game_view_idx].apply_move_dir(SnakeGame.MOVE_DOWN)
 			if key == arcade.key.RIGHT:
-				self.snake_games[self.game_view_idx].apply_move_dir(SnakeGame.MOVE_RIGHT)
+				self.agent.snake_games[self.game_view_idx].apply_move_dir(SnakeGame.MOVE_RIGHT)
 		else:
 			if key == arcade.key.LEFT:
-				self.game_view_idx = (self.game_view_idx - 1) % len(self.snake_games)
+				self.game_view_idx = (self.game_view_idx - 1) % len(self.agent.snake_games)
 			if key == arcade.key.RIGHT:
-				self.game_view_idx = (self.game_view_idx + 1) % len(self.snake_games)
+				self.game_view_idx = (self.game_view_idx + 1) % len(self.agent.snake_games)
 			if key == arcade.key.H:
 				view_idx = 0
 				best_score = 0
-				for i, game in enumerate(self.snake_games):
+				for i, game in enumerate(self.agent.snake_games):
 					if calc_score(game) > best_score:
 						view_idx = i
 						best_score = calc_score(game)
@@ -263,8 +337,8 @@ class MyWindow(arcade.Window):
 				self.game_view_idx = view_idx
 			if key == arcade.key.N:
 				j = self.game_view_idx
-				for i in  range(len(self.snake_games)-1):
-					idx = (j + i) % len(self.snake_games)
+				for i in  range(len(self.agent.snake_games)-1):
+					idx = (j + i) % len(self.agent.snake_games)
 					if game.is_game_over == False:
 						self.game_view_idx = i
 						break
@@ -282,64 +356,18 @@ class MyWindow(arcade.Window):
 			apply_step = True
 
 		if apply_step:
-			start = time.time()
 			if COMPUTER_CONTROLLED:
 				self.apply_manual_step = False
-				input_matrix = torch.zeros((len(self.snake_games), 7)).float()
-				for i, game in enumerate(self.snake_games):
-					inputs = torch.tensor([game.food_pos[0] - game.snake[0][0], game.food_pos[1] - game.snake[0][1], game.snake_move_dir, 0, 0, 0, 0]).float()
-					obstacle_directions = game.get_obstacle_directions()
-					input_move_offset = 3
-					for j in range(4):
-						inputs[j+input_move_offset] = obstacle_directions[j]
-					input_matrix[i] = inputs
-				nn_out = self.neuralnet(input_matrix)
-				#actions = torch.max(nn_out, 1)[1]
-				#probability distribution function
-				pdf = torch.distributions.Categorical(nn_out)
-				actions = pdf.sample()
-				#actions = explore_actions.clone()
-				#for aidx, _ in enumerate(actions):
-				#	if random.random() > 0.25:
-				#		actions[aidx] = nn_out[aidx].max()
-				rewards = torch.zeros(len(self.snake_games))
-			for i, game in enumerate(self.snake_games):
-				game.apply_move_dir(actions[i])
-				is_game_over = game.step()
-				self.max_snake_length = max(self.max_snake_length, len(game.snake))
-				self.max_steps = max(game.steps, self.max_steps)
-				if COMPUTER_CONTROLLED:
-					rewards[i] = calc_score(game)
-				if is_game_over:
-					if USER_INPUT_CONTROLLED and self.is_retry_button_shown == False:
-						restart_button = arcade.gui.UIFlatButton(text="Play Again?", x=self.width/2-100, y=100, width=200, height=50)
-						restart_button.on_click = self.on_restart
-						self.is_retry_button_shown = True
-						self.manager.add(restart_button)
-					if COMPUTER_CONTROLLED:
-						self.snake_games[i] = SnakeGame(BOARD_WIDTH, BOARD_HEIGHT)
-						self.num_restarts += 1
-						self.steps_accum += game.steps
-						avg_steps = 0
-						if self.num_restarts % (NUM_GAMES * 10) == 0:
-							avg_steps = self.steps_accum / (NUM_GAMES * 10)
-							self.steps_accum = 0
-							data = {"steps": avg_steps, "restart":self.num_restarts}
-							json.dump(data, self.steps_progress_file)
-							self.steps_progress_file.write('\n')
-							self.steps_progress_file.flush()
-						if self.num_restarts % (NUM_GAMES * 100) == 0:
-							torch.save(self.neuralnet.state_dict(), SNAKE_MODEL_FILEPATH)
-			if COMPUTER_CONTROLLED:
-				loss = (-pdf.log_prob(actions) * rewards).sum()
-				loss.backward()
-				self.optimizer.step()
-				self.optimizer.zero_grad()
-				print("possible actions: {}, chosen actions: {}. rewards: {}".format(nn_out, actions, rewards))
+			needs_restart = self.agent.update()
+			if needs_restart and self.is_retry_button_shown == False:
+				restart_button = arcade.gui.UIFlatButton(text="Play Again?", x=self.width/2-100, y=100, width=200, height=50)
+				restart_button.on_click = self.on_restart
+				self.is_retry_button_shown = True
+				self.manager.add(restart_button)
 	
 	def on_restart(self, event):
 		self.time_accum = 0
-		self.snake_games[self.game_view_idx] = SnakeGame(BOARD_WIDTH, BOARD_HEIGHT)
+		self.agent.snake_games[self.game_view_idx] = SnakeGame(BOARD_WIDTH, BOARD_HEIGHT)
 		self.is_retry_button_shown = False
 		self.manager.clear()
 
@@ -363,18 +391,18 @@ class MyWindow(arcade.Window):
 				x_start = board_x_start + i*PIXELS_PER_BOARD_TILE
 				y_start = board_y_start + j*PIXELS_PER_BOARD_TILE
 				color = (0,0,0)
-				if i == self.snake_games[self.game_view_idx].snake[0][0] and j == self.snake_games[self.game_view_idx].snake[0][1]:
+				if i == self.agent.snake_games[self.game_view_idx].snake[0][0] and j == self.agent.snake_games[self.game_view_idx].snake[0][1]:
 					color = (104, 0, 182)
-				elif self.snake_games[self.game_view_idx].board[j][i] == SnakeGame.TILE_SNAKE:
+				elif self.agent.snake_games[self.game_view_idx].board[j][i] == SnakeGame.TILE_SNAKE:
 					color = (0, 0, 0)
-				elif self.snake_games[self.game_view_idx].board[j][i] == SnakeGame.TILE_FOOD:
+				elif self.agent.snake_games[self.game_view_idx].board[j][i] == SnakeGame.TILE_FOOD:
 					color = (233, 30, 54)
 				else:
 					continue
 				arcade.draw_lrtb_rectangle_filled(x_start, x_start+PIXELS_PER_BOARD_TILE, y_start+PIXELS_PER_BOARD_TILE, y_start, color)
-		obstacle_directions = self.snake_games[self.game_view_idx].get_obstacle_directions()
-		head_x = self.snake_games[self.game_view_idx].snake[0][0]
-		head_y = self.snake_games[self.game_view_idx].snake[0][1]
+		obstacle_directions = self.agent.snake_games[self.game_view_idx].get_obstacle_directions()
+		head_x = self.agent.snake_games[self.game_view_idx].snake[0][0]
+		head_y = self.agent.snake_games[self.game_view_idx].snake[0][1]
 		obstacle_directions_color = (128, 0, 128, 64)
 		for j in range(1,obstacle_directions[SnakeGame.MOVE_UP]+1):
 			x = board_x_start + head_x * PIXELS_PER_BOARD_TILE
@@ -395,13 +423,13 @@ class MyWindow(arcade.Window):
 		self.manager.draw()
 		
 		arcade.draw_text("FPS: {}".format(self.fps), start_x=0, start_y=0, color=(0,0,0), font_size=16)
-		arcade.draw_text("Score: {}".format(len(self.snake_games[self.game_view_idx].snake)), start_x=0, start_y=self.height-24, color=(0,0,0), font_size=16)
-		arcade.draw_text("Hunger Countdown: {}".format(self.snake_games[self.game_view_idx].food_countdown), start_x=0, start_y=self.height-48, color=(0,0,0), font_size=16)
-		arcade.draw_text("Time Step: {}".format(self.snake_games[self.game_view_idx].steps), start_x=0, start_y=self.height-72, color=(0,0,0), font_size=16)
+		arcade.draw_text("Score: {}".format(len(self.agent.snake_games[self.game_view_idx].snake)), start_x=0, start_y=self.height-24, color=(0,0,0), font_size=16)
+		arcade.draw_text("Hunger Countdown: {}".format(self.agent.snake_games[self.game_view_idx].food_countdown), start_x=0, start_y=self.height-48, color=(0,0,0), font_size=16)
+		arcade.draw_text("Time Step: {}".format(self.agent.snake_games[self.game_view_idx].steps), start_x=0, start_y=self.height-72, color=(0,0,0), font_size=16)
 		arcade.draw_text("Snake Game: {}".format(self.game_view_idx), start_x=0, start_y=self.height-96, color=(0,0,0), font_size=16)
-		arcade.draw_text("Max Steps: {}".format(self.max_steps), start_x=0, start_y=self.height-120, color=(0,0,0), font_size=16)
-		arcade.draw_text("Max Snake Length: {}".format(self.max_snake_length), start_x=0, start_y=self.height-144, color=(0,0,0), font_size=16)
-		arcade.draw_text("Restarts: {}".format(self.num_restarts), start_x=0, start_y=self.height-168, color=(0,0,0), font_size=16)
+		arcade.draw_text("Max Steps: {}".format(self.agent.max_steps), start_x=0, start_y=self.height-120, color=(0,0,0), font_size=16)
+		arcade.draw_text("Max Snake Length: {}".format(self.agent.max_snake_length), start_x=0, start_y=self.height-144, color=(0,0,0), font_size=16)
+		arcade.draw_text("Restarts: {}".format(self.agent.num_restarts), start_x=0, start_y=self.height-168, color=(0,0,0), font_size=16)
 
 def calc_score(game: SnakeGame):
 	if game.is_game_over:
@@ -413,12 +441,19 @@ def calc_score(game: SnakeGame):
 	return -(current_dist - prev_dist)
 
 def main():
-	window = MyWindow()
-	window.set_update_rate(1/60)
+	os.makedirs("./data/progress", exist_ok=True)
 	if (INITIAL_SCREEN_WIDTH < BOARD_WIDTH * PIXELS_PER_BOARD_TILE or INITIAL_SCREEN_HEIGHT < BOARD_HEIGHT * PIXELS_PER_BOARD_TILE):
 		print("WARNING: screen width and screen height should be at least {} and {} respectively to fill the whole board".format(BOARD_WIDTH * PIXELS_PER_BOARD_TILE, BOARD_HEIGHT * PIXELS_PER_BOARD_TILE))
-	window.setup()
-	arcade.run()
+
+	if HEADLESS:
+		agent = AgentSim(True)
+		while True:
+			agent.update()
+	else:
+		window = MyWindow()
+		window.set_update_rate(1/144)
+		window.setup()
+		arcade.run()
 
 if __name__ == "__main__":
 	main()
